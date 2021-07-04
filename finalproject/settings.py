ec511131bf257c57dbf1b8542be3b7f0b6730e4e
Will <51788311+Willyiam723@users.noreply.github.com>
2021-07-04 13:30:05 -0400

    Add/adjust files for heroku deploy

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$csb+ri+fqa&^j=*!$ztd(6b(web))1sjs*=ug(vg+a5d6^r@^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scenarios-analysis.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
